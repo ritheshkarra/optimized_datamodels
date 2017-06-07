db4b6d8104993942242c8602771c8a07f9f73feb
Rithesh <rithesh.karra@mtuity.com>
2017-06-07 12:07:46 +0530

    changes to code

--- app/models/environment/endpoints.py
+++ app/models/environment/endpoints.py
@@ -5,7 +5,7 @@ import datetime
 import os
 import numpy as np
 import psycopg2
-#from server.logs import logger
+from server.logs import logger
 
 class So2Predictions(object):
     def on_get(self, req, resp):
@@ -26,16 +26,24 @@ class So2Predictions(object):
         if len(models.keys()) == 0:        
            ##Falcon HTTP Exception handling loop begins http://falcon.readthedocs.io/en/stable/api/errors.html
            logger.info('No data available for location {}.'.format(location))
-           raise falcon.HTTPError(status="204 Data Not Avilable",title='SO2 Predictions',description='No data available',code=204)
+           resp.status = falcon.HTTP_204
+           resp.body= json.dumps({"Status Code":204,"Description":"Data not available","title":"SO2 Predictions"})
+           #raise falcon.HTTPError(status="204 Data Not Avilable",title='SO2 Predictions',description='No data available',code=204)
      except KeyError as e:
          logger.error('Invalid So2 Request'.format(e))
-         raise falcon.HTTPError(status="400 Bad Request",title='SO2 Predictions',description='Invalid Request',code=400)
+         resp.status = falcon.HTTP_400
+         resp.body= json.dumps({"Status Code":400,"Description":"Invalid Request","title":"SO2 Predictions"})
+         #raise falcon.HTTPError(status="400 Bad Request",title='SO2 Predictions',description='Invalid Request',code=400)
      except FileNotFoundError as e:                                     #Raises exception, if pickle file not found
         logger.error('Error loading pickle file.{}'.format(e))
-        raise falcon.HTTPError(status="404 Not found",title='SO2 Predictions',description='Not Found',code='404')
+        resp.status = falcon.HTTP_404
+        resp.body= json.dumps({"Status Code":404,"Description":"File not found","title":"SO2 Predictions"})
+        #raise falcon.HTTPError(status="404 Not found",title='SO2 Predictions',description='Not Found',code='404')
      except Exception as e:
          logger.error('Unknown error at So2 Predictions.{}'.format(e))
-         raise falcon.HTTPError(status="500 Internal Server Error",title='SO2 Predictions',description='Internal Server Error',code='500')
+         resp.status = falcon.HTTP_500
+         resp.body= json.dumps({"Status Code":500,"Description":"Internal Server Error","title":"SO2 Predictions"})
+         #raise falcon.HTTPError(status="500 Internal Server Error",title='SO2 Predictions',description='Internal Server Error',code='500')
      forecast ={}
      for sensid in models.keys():
          if sensorid == "all":    #calling get_parkingforecast
@@ -66,16 +74,24 @@ class HumidityPredictions(object):
         models = pickle.load(open(filePath, 'rb'))           #Loading the pickle file
         if len(models.keys()) == 0:
            logger.info('No data available for location {}.'.format(location))
-           raise falcon.HTTPError(status="204 Data Not Avilable",title='Humidity Predictions',description='No data available',code=204)
+           resp.status = falcon.HTTP_204
+           resp.body= json.dumps({"Status Code":204,"Description":"Data not available","title":"Humidity Predictions"})
+           #raise falcon.HTTPError(status="204 Data Not Avilable",title='Humidity Predictions',description='No data available',code=204)
      except KeyError as e:
         logger.error('Invalid HumidityPredictions Request.{}'.format(e))
-        raise falcon.HTTPError(status="400 Bad Request",title='Humidity Predictions',description='Invalid Request',code=400)
+        resp.status = falcon.HTTP_400
+        resp.body= json.dumps({"Status Code":400,"Description":"Invalid Request","title":"Humidity Predictions"})
+        #raise falcon.HTTPError(status="400 Bad Request",title='Humidity Predictions',description='Invalid Request',code=400)
      except FileNotFoundError as e:
         logger.error('Error loading pickle file.{}'.format(e))
-        raise falcon.HTTPError(status="404 Not found",title='Humidity Predictions',description='Not Found',code='404')
+        resp.status = falcon.HTTP_404
+        resp.body= json.dumps({"Status Code":404,"Description":"File not found","title":"Parking Predictions"})
+        #raise falcon.HTTPError(status="404 Not found",title='Humidity Predictions',description='Not Found',code='404')
      except Exception as e:
         logger.error('Error at So2 predictions.{}'.format(e))
-        raise falcon.HTTPError(status="500 Internal Server Error",title='Humidity Predictions',description='Internal Server Error',code='500')
+        resp.status = falcon.HTTP_500
+        resp.body= json.dumps({"Status Code":500,"Description":"Internal Server Error","title":"Humidity Predictions"})
+        #raise falcon.HTTPError(status="500 Internal Server Error",title='Humidity Predictions',description='Internal Server Error',code='500')
 
      forecast ={}
      for sensid in models.keys():
@@ -107,16 +123,24 @@ class AmbientTempPredictions(object):
         models = pickle.load(open(filePath, 'rb'))
         if len(models.keys()) == 0:
            logger.info('No data available for location {}.'.format(location))
-           raise falcon.HTTPError(status="204 Data Not Avilable",title='Ambient Temperature',description='No data available',code=204)
+           resp.status = falcon.HTTP_204
+           resp.body= json.dumps({"Status Code":204,"Description":"Data not available","title":"AmbientTemperature Predictions"})
+           #raise falcon.HTTPError(status="204 Data Not Avilable",title='Ambient Temperature',description='No data available',code=204)
      except KeyError as e:
         logger.error('Invalid Ambient Temperature Request.{}'.format(e))
-        raise falcon.HTTPError(status="400 Bad Request",title='Ambient Temperature',description='Invalid Request',code=400)
+        resp.status = falcon.HTTP_400
+        resp.body= json.dumps({"Status Code":400,"Description":"Invalid Request","title":"AmbientTemperature Predictions"})
+        #raise falcon.HTTPError(status="400 Bad Request",title='Ambient Temperature',description='Invalid Request',code=400)
      except FileNotFoundError as e:
         logger.error('Error loading pickle file.{}'.format(e))
-        raise falcon.HTTPError(status="404 Not found",title='Ambient Temperature',description='Not Found',code='404')
+        resp.status = falcon.HTTP_404
+        resp.body= json.dumps({"Status Code":404,"Description":"File not found","title":"AmbientTemperature Predictions"})
+        #raise falcon.HTTPError(status="404 Not found",title='Ambient Temperature',description='Not Found',code='404')
      except Exception as e:
         logger.error('Error at Ambient Temperature predictions.{}'.format(e))
-        raise falcon.HTTPError(status="500 Internal Server Error",title='Ambient Temperature',description='Internal Server Error',code='500')
+        resp.status = falcon.HTTP_500
+        resp.body= json.dumps({"Status Code":500,"Description":"Internal Server Error","title":"Ambient TemperaturePredictions"})
+        #raise falcon.HTTPError(status="500 Internal Server Error",title='Ambient Temperature',description='Internal Server Error',code='500')
 
      forecast ={}
      for sensid in models.keys():
